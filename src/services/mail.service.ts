68516c95d04fb3915bb55c6f0bbdfd46a8ead859
Devdeep Ghosh <devarmforce01@gmail.com>
2024-01-23 16:27:52 +0530

    mailservice updated

---
+++ src/services/mail.service.ts
@@ -0,0 +1,72 @@
+import nodemailer from "nodemailer";
+import logger from "../config/logger";
+
+const transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+if (process.env.NODE_ENV !== "test") {
+  transport
+    .verify()
+    .then(() => logger.info("Connected to email server"))
+    .catch(() =>
+      logger.warn(
+        "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
+      )
+    );
+}
+
+/**
+ * Send an email
+ * @param {string} to
+ * @param {string} subject
+ * @param {string} text
+ * @returns {Promise}
+ */
+const sendEmail = async (to: string, subject: string, text: string) => {
+  const msg = { from: process.env.SMTP_SENDER, to, subject, text };
+  await transport.sendMail(msg);
+};
+
+/**
+ * Send reset password email
+ * @param {string} to
+ * @param {string} token
+ * @returns {Promise}
+ */
+const sendResetPasswordEmail = async (to: string, token: string) => {
+  const subject = "Reset password";
+  // replace this url with the link to the reset password page of your front-end app
+  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+  const text = `Dear user,
+To reset your password, click on this link: ${resetPasswordUrl}
+If you did not request any password resets, then ignore this email.`;
+  await sendEmail(to, subject, text);
+};
+
+/**
+ * Send verification email
+ * @param {string} to
+ * @param {string} token
+ * @returns {Promise}
+ */
+const sendVerificationEmail = async (to: string, token: string) => {
+  const subject = "Email Verification";
+  // replace this url with the link to the email verification page of your front-end app
+  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
+  const text = `Dear user,
+To verify your email, click on this link: ${verificationEmailUrl}`;
+  await sendEmail(to, subject, text);
+};
+
+export default {
+  transport,
+  sendEmail,
+  sendResetPasswordEmail,
+  sendVerificationEmail,
+};
